a318d08f4023aa9e74a67638e865eae22ba4e149
emunhoz <edermunhozsantos@gmail.com>
2021-02-15 01:59:44 -0300

    chore: add background in storybook stories

--- packages/ui-components/.storybook/preview.js
+++ packages/ui-components/.storybook/preview.js
@@ -3,7 +3,16 @@ import { ThemeProvider } from 'styled-components'
 import createTheme from '@monorepo/design-tokens'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" }
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'significa-invision',
+    values: [
+      {
+        name: 'significa-invision',
+        value: '#0a0f14',
+      },
+    ]
+  }
 }
 
 export const decorators = [
