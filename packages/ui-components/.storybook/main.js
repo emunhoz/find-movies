6e673a33704ea68a54b665adff49b5c7d9713d99
emunhoz <edermunhozsantos@gmail.com>
2021-02-17 03:41:23 -0300

    chore: remove svg loader from storybook

--- packages/ui-components/.storybook/main.js
+++ packages/ui-components/.storybook/main.js
@@ -1,16 +1,4 @@
 module.exports = {
-  webpackFinal: async (config, { configType }) => {
-    const rules = config.module.rules;
-    const fileLoaderRule = rules.find(rule => rule.test.test('.svg'));
-    fileLoaderRule.exclude = /\.svg$/;
-
-    rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
