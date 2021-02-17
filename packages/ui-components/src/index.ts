7396a4d0f48567a18d4c22fe265e2cd833432969
emunhoz <edermunhozsantos@gmail.com>
2021-02-17 03:42:22 -0300

    chore: export all components in ui-components package

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,1 +1,5 @@
 export * from './components/button/Button'
+export * from './components/label-with-logo/LabelWithLogo'
+export * from './components/layout/Layout'
+export * from './components/movie-card/MovieCard'
+export * from './components/search-bar/SearchBar'
