c8e20de97787fd9e9ed1dc7d548a3267626c504a
emunhoz <edermunhozsantos@gmail.com>
2021-02-18 09:33:20 -0300

    chore: add function to get movie by id

--- packages/app/src/services/movies.ts
+++ packages/app/src/services/movies.ts
@@ -1,3 +1,5 @@
 import HTTP_CLIENT from './api'
 
 export const findMoviesByName = (name: string) => HTTP_CLIENT.get('', { params: { s: name }})
+
+export const findMoviesById = (id: string) => HTTP_CLIENT.get('', { params: { i: id }})
