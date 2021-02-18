b1410d070d0e89e091439c5f511add30dfba7dcd
emunhoz <edermunhozsantos@gmail.com>
2021-02-18 09:32:42 -0300

    chore: add icon arrow white

--- packages/commons/images/index.ts
+++ packages/commons/images/index.ts
@@ -2,6 +2,7 @@ import IconHeart from './icons/icon-heart.svg'
 import IconHeartFull from './icons/icon-heart-full.svg'
 import IconHeartWhite from './icons/icon-heart-white.svg'
 import SearchIcon from './icons/icon-magnifier-white.svg'
+import ArrowWhite from './icons/icon-arrow-back.svg'
 import EmptyState from './illustrations/empty-state.png'
 import ImageNotFound from './illustrations/image-not-found.svg'
 import Logo from './logos/logo.svg'
@@ -11,6 +12,7 @@ export {
   IconHeartFull,
   IconHeartWhite,
   SearchIcon,
+  ArrowWhite,
   EmptyState,
   ImageNotFound,
   Logo
