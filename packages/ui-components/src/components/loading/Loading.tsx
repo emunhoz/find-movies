3ccc43068aad1973733a2fa795665190ad601e79
emunhoz <edermunhozsantos@gmail.com>
2021-02-18 05:00:37 -0300

    chore: add attr data-testid in loading component

--- packages/ui-components/src/components/loading/Loading.tsx
+++ packages/ui-components/src/components/loading/Loading.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import * as S from './Loading.style'
 
-export const Loading = () => <S.Loading><div className="spinner"></div></S.Loading>
+export const Loading = () => <S.Loading><div className="spinner" data-testid="loading"></div></S.Loading>
