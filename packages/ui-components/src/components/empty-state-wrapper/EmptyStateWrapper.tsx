83a273ca61c5c3b87e7b56aa8464817974105b27
emunhoz <edermunhozsantos@gmail.com>
2021-02-19 00:25:37 -0300

    perf: improvement empty state image

--- packages/ui-components/src/components/empty-state-wrapper/EmptyStateWrapper.tsx
+++ packages/ui-components/src/components/empty-state-wrapper/EmptyStateWrapper.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import { EmptyState } from '@commons/images'
 import * as S from './EmptyStateWrapper.style'
 
-interface IEmptyStateWrapper {
-  example?: string
-}
-
-export const EmptyStateWrapper = ({ example = 'example' }: IEmptyStateWrapper) => (
+export const EmptyStateWrapper = () => (
   <S.EmptyStateWrapper>
-    <S.EmptyStateImg src={EmptyState} alt="Empty state" />
+    <S.EmptyStateImg loading="lazy" width="400" height="200" src={EmptyState} alt="Empty state" />
     <S.EmptyStateTitle>Don&apos;t know what to search?</S.EmptyStateTitle>
     <S.EmptyStateSubTitle>Here&apos;s an offer you can&apos;t refuse</S.EmptyStateSubTitle>
   </S.EmptyStateWrapper>
