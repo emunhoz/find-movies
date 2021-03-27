c4d4540e69fb9f76d8fd7871047a26dfe7b01f69
emunhoz <edermunhozsantos@gmail.com>
2021-03-27 16:05:16 -0300

    refactor: improvement LabelWithLogo style

--- packages/ui-components/src/components/label-with-logo/LabelWithLogo.style.tsx
+++ packages/ui-components/src/components/label-with-logo/LabelWithLogo.style.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const LabelWithLogo = styled.div`
   display: inline-flex;
+  min-height: 44px;
   overflow: hidden;
   border: 1px solid ${({ theme }) => theme.palette.midGrey};
   border-radius: ${({ theme }) => theme.attributes.borderRadius.small}px;
@@ -10,13 +11,15 @@ export const LabelWithLogo = styled.div`
 export const LogoWrapper = styled.div<{ backgroundLogo: string }>`
   display: flex;
   align-items: center;
-  min-width: 24px;
+  justify-content: center;
+  width: 38px;
   padding: 8px;
   background: ${({ backgroundLogo }) => backgroundLogo || 'none'};
 `
 
 export const Logo = styled.img`
-  max-height: 18px;
+  width: 100%;
+  max-width: 35px;
 `
 
 export const Value = styled.div`
